efc143ff177dd19aa38013aa4124c1e0c5985186
Devanshukoli <kolidevanshu02@gmail.com>
2026-05-15 13:49:55 +0530

    remove hardcoded info of meme

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react"
+
 const Main = () => {
+
+  const [memeInputs, setMemeInputs] = useState({
+    topText: "One does not simply",
+    bottomText: "walk into modor",
+    imageUrl: "http://i.imgflip.com/1bij.jpg"
+  })
+
   return (
     <>
       <main>
@@ -6,7 +15,7 @@ const Main = () => {
           <label>Top Text
             <input
               type="text"
-              placeholder="One does not simply"
+              placeholder={memeInputs.topText}
               name="topText"
             />
           </label>
@@ -14,14 +23,14 @@ const Main = () => {
           <label>Bottom Text
             <input
               type="text"
-              placeholder="Walk into Mordor"
+              placeholder={memeInputs.bottomText}
               name="bottomText"
             />
           </label>
           <button>Get a new meme image 🖼</button>
         </div>
         <div className="meme">
-          <img src="http://i.imgflip.com/1bij.jpg" />
+          <img src={memeInputs.imageUrl} />
           <span className="top">One does not simply</span>
           <span className="bottom">Walk into Mordor</span>
         </div>
